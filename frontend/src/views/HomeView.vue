d964e3c4d5edc478594f3fed94f7fea8977a9ebd
Matthew Thornton <matt.thornton@knights.ucf.edu>
2022-12-05 14:40:39 -0500

    Refactor out the form to use NewTaskForm component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,62 @@
+<template>
+  <main>
+    <FieldSet legend="New Task" :toggleable="true" :collapsed="true">
+      <NewTaskForm @new-task="newTask" />
+    </FieldSet>
+    <FieldSet
+      legend="Daily"
+      :toggleable="true"
+      :collapsed="dailyTasks.length === 0"
+    >
+      <TaskList @remove-task="removeTask" :tasks="dailyTasks" />
+    </FieldSet>
+    <FieldSet
+      legend="Weekly"
+      :toggleable="true"
+      :collapsed="weeklyTasks.length === 0"
+    >
+      <TaskList @remove-task="removeTask" :tasks="weeklyTasks" />
+    </FieldSet>
+    <FieldSet
+      legend="Monthly"
+      :toggleable="true"
+      :collapsed="monthlyTasks.length === 0"
+    >
+      <TaskList @remove-task="removeTask" :tasks="monthlyTasks" />
+    </FieldSet>
+    <FieldSet
+      legend="Quarterly"
+      :toggleable="true"
+      :collapsed="quarterlyTasks.length === 0"
+    >
+      <TaskList @remove-task="removeTask" :tasks="quarterlyTasks" />
+    </FieldSet>
+    <FieldSet
+      legend="Yearly"
+      :toggleable="true"
+      :collapsed="yearlyTasks.length === 0"
+    >
+      <TaskList @remove-task="removeTask" :tasks="yearlyTasks" />
+    </FieldSet>
+    <TheWelcome />
+  </main>
+</template>
+
 <script setup lang="ts">
 import { computed, onMounted, ref, type Ref } from "vue";
 import { TaskType } from "@/enums/tasktype.enum";
 import type ITask from "@/interfaces/task.interface";
+import type INewTaskForm from "@/interfaces/newTaskForm.interface";
 import type INewTaskDTO from "@/dtos/newtask.dto";
 import TaskService from "@/services/task.service";
 import TheWelcome from "../components/TheWelcome.vue";
 import FieldSet from "primevue/fieldset";
 import TaskList from "@/components/TaskList.vue";
-import Button from "primevue/button";
-import InputText from "primevue/inputtext";
-import Dropdown from "primevue/dropdown";
-import Calendar from "primevue/calendar";
+import NewTaskForm from "@/components/NewTaskForm.vue";
 import { DateTime } from "luxon";
+import { useActionStore } from "@/stores/action";
 
+const store = useActionStore();
 const taskService: TaskService = new TaskService();
 const tasks: Ref<Array<ITask>> = ref([]);
 
@@ -26,68 +70,34 @@ const dailyTasks = computed(() =>
 const weeklyTasks = computed(() =>
   tasks.value.filter((task: ITask) => task.type === TaskType.WEEKLY)
 );
+const monthlyTasks = computed(() =>
+  tasks.value.filter((task: ITask) => task.type === TaskType.MONTHLY)
+);
+const quarterlyTasks = computed(() =>
+  tasks.value.filter((task: ITask) => task.type === TaskType.QUARTERLY)
+);
+const yearlyTasks = computed(() =>
+  tasks.value.filter((task: ITask) => task.type === TaskType.ANNUALLY)
+);
 
-function newTask() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function newTask(newTask: INewTaskForm) {
   const task: INewTaskDTO = {
-    title: title.value,
-    description: description.value !== "" ? description.value : undefined,
-    type: taskService.enumFromValue(tasktype.value, TaskType),
-    due: DateTime.fromJSDate(due.value),
+    title: newTask.title,
+    description: newTask.description,
+    type: taskService.enumFromValue(newTask.taskType, TaskType),
+    due: DateTime.fromJSDate(newTask.due),
   };
+  tasks.value.push(await taskService.newTask(task));
 }
 
-const tasktypes = ref(["Daily", "Weekly", "Monthly", "Quarterly", "Yearly"]);
-
-const title = ref("");
-const description = ref("");
-const tasktype = ref("");
-const due = ref();
+async function removeTask(id: string) {
+  await taskService.deleteTask(id);
+  const index = tasks.value.findIndex((task: ITask) => task.id === id);
+  if (index > -1) {
+    tasks.value.splice(index, 1);
+  }
+}
 </script>
-
-<template>
-  <main>
-    <FieldSet
-      class="dark"
-      legend="New Task"
-      :toggleable="true"
-      :collapsed="true"
-    >
-      <div class="form">
-        <label for="title" class="label">Title</label>
-        <InputText id="title" type="text" v-model="title" />
-        <label for="description" class="label">Description</label>
-        <InputText id="description" type="text" v-model="description" />
-        <label for="tasktype" class="label">Period</label>
-        <Dropdown
-          id="tasktype"
-          v-model="tasktype"
-          :options="tasktypes"
-          placeholder="Select period"
-        />
-        <label for="due" class="label">Due Date</label>
-        <Calendar inputId="due" v-model="due" autocomplete="off" />
-        <Button icon="pi pi-plus" @click="newTask" label="Submit" />
-      </div>
-    </FieldSet>
-    <FieldSet
-      class="dark"
-      legend="Daily"
-      :toggleable="true"
-      :collapsed="dailyTasks.length === 0"
-    >
-      <TaskList :tasks="dailyTasks" />
-    </FieldSet>
-    <FieldSet
-      legend="Weekly"
-      :toggleable="true"
-      :collapsed="weeklyTasks.length === 0"
-    >
-      <TaskList :tasks="weeklyTasks" />
-    </FieldSet>
-    <TheWelcome />
-  </main>
-</template>
 
 <style lang="scss" scoped>
 .p-fieldset {
@@ -97,16 +107,6 @@ const due = ref();
   }
   :deep(.p-fieldset-legend-text) {
     color: white;
-  }
-}
-.form {
-  display: flex;
-  flex-direction: column;
-  .label {
-    font-size: x-large;
-    font-weight: 600;
-    margin-right: 1rem;
-    color: whitesmoke;
   }
 }
 </style>
