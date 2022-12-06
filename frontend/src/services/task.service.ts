754ba5b18d2494b23335f4c4fec9c9c848f48ed5
Matthew Thornton <matt.thornton@knights.ucf.edu>
2022-12-06 13:20:50 -0500

    Feature to update tasks

--- frontend/src/services/task.service.ts
+++ frontend/src/services/task.service.ts
@@ -1,6 +1,7 @@
 import { TaskType } from "@/enums/tasktype.enum";
 import type ITask from "@/interfaces/task.interface";
 import type INewTaskDTO from "@/dtos/newtask.dto";
+import type IUpdateTaskDTO from "@/dtos/updatetask.dto";
 import { DateTime } from "luxon";
 
 export default class TaskService {
@@ -43,6 +44,32 @@ export default class TaskService {
       });
     }
     return taskList;
+  }
+
+  public async updateTask(task: IUpdateTaskDTO): Promise<ITask> {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...body } = task;
+    const response = await fetch(`http://localhost:3000/v1/tasks/${task.id}`, {
+      method: "PATCH",
+      headers: {
+        //Authorization: `Bearer ${window.localStorage.token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (response.status === 200) {
+      const taskFromServer = await response.json();
+      return {
+        id: taskFromServer._id,
+        title: taskFromServer.title,
+        type: this.enumFromValue(taskFromServer.type, TaskType),
+        due: DateTime.fromISO(taskFromServer.due),
+        completed: taskFromServer.complete,
+        lastUpdated: DateTime.fromISO(taskFromServer.lastUpdated),
+      };
+    }
+    throw new Error("Unknown error occurred");
   }
 
   public async deleteTask(id: string): Promise<ITask> {
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,42 +2,69 @@
   <main>
     <FieldSet legend="New Task" :toggleable="true" :collapsed="true">
       <NewTaskForm @new-task="newTask" />
+      <UpdateTaskForm
+        :visible="updateTaskVisible"
+        :task="taskToUpdate"
+        @close="updateTaskVisible = false"
+        @update-task="requestUpdateTask"
+      />
     </FieldSet>
     <FieldSet
       legend="Daily"
       :toggleable="true"
       :collapsed="dailyTasks.length === 0"
     >
-      <TaskList @remove-task="removeTask" :tasks="dailyTasks" />
+      <TaskList
+        @remove-task="removeTask"
+        @update-task="updateTask"
+        :tasks="dailyTasks"
+      />
     </FieldSet>
     <FieldSet
       legend="Weekly"
       :toggleable="true"
       :collapsed="weeklyTasks.length === 0"
     >
-      <TaskList @remove-task="removeTask" :tasks="weeklyTasks" />
+      <TaskList
+        @remove-task="removeTask"
+        @update-task="updateTask"
+        :tasks="weeklyTasks"
+      />
     </FieldSet>
     <FieldSet
       legend="Monthly"
       :toggleable="true"
       :collapsed="monthlyTasks.length === 0"
     >
-      <TaskList @remove-task="removeTask" :tasks="monthlyTasks" />
+      <TaskList
+        @remove-task="removeTask"
+        @update-task="updateTask"
+        :tasks="monthlyTasks"
+      />
     </FieldSet>
     <FieldSet
       legend="Quarterly"
       :toggleable="true"
       :collapsed="quarterlyTasks.length === 0"
     >
-      <TaskList @remove-task="removeTask" :tasks="quarterlyTasks" />
+      <TaskList
+        @remove-task="removeTask"
+        @update-task="updateTask"
+        :tasks="quarterlyTasks"
+      />
     </FieldSet>
     <FieldSet
       legend="Yearly"
       :toggleable="true"
       :collapsed="yearlyTasks.length === 0"
     >
-      <TaskList @remove-task="removeTask" :tasks="yearlyTasks" />
+      <TaskList
+        @remove-task="removeTask"
+        @update-task="updateTask"
+        :tasks="yearlyTasks"
+      />
     </FieldSet>
+    <DynamicDialog />
     <TheWelcome />
   </main>
 </template>
@@ -47,18 +74,24 @@ import { computed, onMounted, ref, type Ref } from "vue";
 import { TaskType } from "@/enums/tasktype.enum";
 import type ITask from "@/interfaces/task.interface";
 import type INewTaskForm from "@/interfaces/newTaskForm.interface";
+import type IUpdateTaskForm from "@/interfaces/updateTaskForm.interface";
 import type INewTaskDTO from "@/dtos/newtask.dto";
+import type IUpdateTaskDTO from "@/dtos/updatetask.dto";
 import TaskService from "@/services/task.service";
 import TheWelcome from "../components/TheWelcome.vue";
 import FieldSet from "primevue/fieldset";
+import DynamicDialog from "primevue/dynamicdialog";
 import TaskList from "@/components/TaskList.vue";
 import NewTaskForm from "@/components/NewTaskForm.vue";
+import UpdateTaskForm from "@/components/UpdateTaskForm.vue";
 import { DateTime } from "luxon";
 import { useActionStore } from "@/stores/action";
 
 const store = useActionStore();
 const taskService: TaskService = new TaskService();
 const tasks: Ref<Array<ITask>> = ref([]);
+const updateTaskVisible = ref(false);
+const taskToUpdate: Ref<ITask | undefined> = ref(undefined);
 
 onMounted(async () => {
   tasks.value = await taskService.getTasks();
@@ -80,7 +113,7 @@ const yearlyTasks = computed(() =>
   tasks.value.filter((task: ITask) => task.type === TaskType.ANNUALLY)
 );
 
-async function newTask(newTask: INewTaskForm) {
+async function newTask(newTask: INewTaskForm): Promise<void> {
   const task: INewTaskDTO = {
     title: newTask.title,
     description: newTask.description,
@@ -90,12 +123,42 @@ async function newTask(newTask: INewTaskForm) {
   tasks.value.push(await taskService.newTask(task));
 }
 
-async function removeTask(id: string) {
+async function requestUpdateTask(taskToUpdate: IUpdateTaskForm): Promise<void> {
+  const task: IUpdateTaskDTO = {
+    id: taskToUpdate.id,
+    title: taskToUpdate.title,
+    description: taskToUpdate.description,
+    type: taskService.enumFromValue(taskToUpdate.taskType, TaskType),
+    due: DateTime.fromJSDate(taskToUpdate.due),
+    complete: taskToUpdate.complete,
+  };
+  const taskFromServer = await taskService.updateTask(task);
+  const cachedTask = tasks.value.find((t) => t.id === taskFromServer.id);
+  if (cachedTask) {
+    cachedTask.completed = taskFromServer.completed;
+    cachedTask.title = taskFromServer.title;
+    cachedTask.due = taskFromServer.due;
+    cachedTask.description = taskFromServer.description;
+    cachedTask.lastUpdated = taskFromServer.lastUpdated;
+    cachedTask.type = taskFromServer.type;
+  }
+  updateTaskVisible.value = false;
+}
+
+async function removeTask(id: string): Promise<void> {
   await taskService.deleteTask(id);
   const index = tasks.value.findIndex((task: ITask) => task.id === id);
   if (index > -1) {
     tasks.value.splice(index, 1);
   }
+}
+
+function updateTask(id: string): void {
+  console.log(`Editing ${id}`);
+  const task = tasks.value.find((t) => t.id === id);
+  if (!task) return;
+  taskToUpdate.value = task;
+  updateTaskVisible.value = true;
 }
 </script>
 
